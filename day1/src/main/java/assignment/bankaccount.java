660d021ec34f699ed7dc3dfcb8a0fd23330210e4
Sakshi <Sakshi@192.168.20.7>
2020-01-28 13:39:44 +1100

    test comit

---
+++ day1/src/main/java/assignment/bankaccount.java
@@ -0,0 +1,86 @@
+//package assignment;
+//
+//public class bankaccount {
+//
+//	int accoiunt_no;
+//	double balance;
+//double withdraw = 0.0;
+//double deposit = 0.0;
+//	String cust_name;
+//	char email;
+//	int phone_no;
+//		
+//	public int getAccoiunt_no() {
+//		System.out.println("enter the account no");
+//		return accoiunt_no;
+//	}
+//	public void setAccoiunt_no(int accoiunt_no) {
+//		this.accoiunt_no = accoiunt_no;
+//	}
+//	public double getBalance() {
+//		System.out.println("enter the balance");
+//		return balance;
+//	}
+//	public void setBalance(double balance) {
+//		this.balance = balance;
+//	}
+//	public String getCust_name() {
+//		System.out.println("enter the customer name");
+//		return cust_name;
+//	}
+//	public void setCust_name(String cust_name) {
+//		this.cust_name = cust_name;
+//	}
+//	public char getEmail() {
+//		System.out.println("enter the email address");
+//		return email;
+//	}
+//	public void setEmail(char email) {
+//		this.email = email;
+//	}
+//	public int getPhone_no() {
+//		System.out.println("enter the phone no");
+//		return phone_no;
+//	}
+//	public void setPhone_no(int phone_no) {
+//		this.phone_no = phone_no;
+//	}
+//	 public double getDeposit()
+//	 {
+//		 System.out.println("enter how much you want to deposite");
+//			 return deposit;
+//	 }
+//	 public void setDeposit(double deposit)
+//	 {
+//		 this.deposit = deposit;
+//	 }
+//	public void calculatedeposite()
+//	{
+//		
+//		deposit = deposit + balance;
+//		System.out.println("deposite is "+deposit);
+//	}
+//	
+//	public double getWithdraw()
+//	{
+//		System.out.println("enter how much to withdraw");
+//		return withdraw;
+//	}
+//	
+//	public void setWithdraw(double withdraw)
+//	{
+//		this.withdraw = withdraw;
+//	}
+//	public void calculatewithdraw()
+//	{
+//     if (balance <=0.0)
+//System.out.println("insufficient balance");
+//balance = balance - withdraw;
+//System.out.println("balance is " +balance);
+//	}
+//
+//	
+//
+//
+//
+//}
